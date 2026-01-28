6465c8ed87b60b2dd2be4678bf9ed5e0ec38aecc
nandini bhawre <nandinibhawre24@gmailcom>
2026-01-28 15:14:00 +0530

    add DashboardRepo

--- src/main/java/com/Spring/elitedemo1/Repository/DashboardRepo.java
+++ src/main/java/com/Spring/elitedemo1/Repository/DashboardRepo.java
@@ -1,4 +1,14 @@
 package com.Spring.elitedemo1.Repository;
 
-public class DashboardRepo {
+import com.Spring.elitedemo1.Model.Dashboard;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DashboardRepo extends MongoRepository<Dashboard,String>
+
+{
+    List<Dashboard> findByUserId(String userId);
 }
