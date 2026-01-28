c6385dfaad1fca443b28fda2a93aebc6f524ddda
nandini bhawre <nandinibhawre24@gmailcom>
2026-01-28 10:51:58 +0530

    add AuthenticationController

--- src/main/java/com/Spring/elitedemo1/Controller/AuthenticationController.java
+++ src/main/java/com/Spring/elitedemo1/Controller/AuthenticationController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin("*")
-public class AuthenticationController {
+public class AuthenticationController
+{
 
     @Autowired
     private AuthenticationServices authService;
