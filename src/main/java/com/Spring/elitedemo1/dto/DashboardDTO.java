95f412cde0b3593d85892cb3a728bb11e947b712
nandini bhawre <nandinibhawre24@gmailcom>
2026-01-28 15:12:34 +0530

    add DashboardDTO

--- src/main/java/com/Spring/elitedemo1/dto/DashboardDTO.java
+++ src/main/java/com/Spring/elitedemo1/dto/DashboardDTO.java
@@ -1,4 +1,18 @@
 package com.Spring.elitedemo1.dto;
 
-public class DashboardDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DashboardDTO
+{
+
+    private String category;
+    private String location;
+    private Double rating;
 }
