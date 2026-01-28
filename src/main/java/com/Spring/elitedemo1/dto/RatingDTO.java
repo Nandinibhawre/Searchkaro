64ca54d55a163dec6d37b4cb2f56a4ec133ac9e1
nandini bhawre <nandinibhawre24@gmailcom>
2026-01-28 17:19:10 +0530

    add RatingDTO

---
+++ src/main/java/com/Spring/elitedemo1/dto/RatingDTO.java
@@ -0,0 +1,17 @@
+package com.Spring.elitedemo1.dto;
+
+public class RatingDTO {
+
+
+    private int rating;
+
+    public RatingDTO(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
+
+
