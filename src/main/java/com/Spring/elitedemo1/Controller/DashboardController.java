8e058851559764b7f86bd07ba7fc3d7dd7ca4579
nandini bhawre <nandinibhawre24@gmailcom>
2026-01-28 15:11:42 +0530

    add DashboardController

--- src/main/java/com/Spring/elitedemo1/Controller/DashboardController.java
+++ src/main/java/com/Spring/elitedemo1/Controller/DashboardController.java
@@ -1,38 +1,47 @@
 package com.Spring.elitedemo1.Controller;
 
 
+import com.Spring.elitedemo1.Services.DashboardServices;
 import com.Spring.elitedemo1.Util.JwtUtil;
+import com.Spring.elitedemo1.dto.DashboardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
-public class protectedController {
+public class DashboardController {
 
     @Autowired
     private JwtUtil jwtService;
-
-    @GetMapping("/protected")
-    public String protectedEndpoint(@RequestHeader("Authorization") String auth) {
+    @Autowired
+    private DashboardServices dashboardService;
+    @GetMapping("/dashboard")
+    public ResponseEntity<List<DashboardDTO>> dashboard(
+            @RequestHeader("Authorization") String auth) {
 
         if (!auth.startsWith("Bearer ")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid token");
         }
 
         String token = auth.substring(7);
 
         if (!jwtService.isTokenValid(token)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Token invalid");
         }
 
-        return "Welcome to Dashboard 🎉";
+        // ✅ userId from JWT
+        String userId = jwtService.extractUserId(token);
+
+        List<DashboardDTO> dashboardData =
+                dashboardService.getDashboardData(userId);
+
+        return ResponseEntity.ok(dashboardData);
     }
 
-//    @GetMapping("/protected")
-//    public String protectedEndpoint() {
-//        return "Welcome to Dashboard 🎉";
-//    }
+
 }
